ae6d8e6a443f3b7cac09dd486b9e8e5c1ba93e07
Malcolm Landon <malcolm.landon@technologyfromsage.com>
2025-01-21 08:12:48 +0000

    Refactor and add test intentianally failing

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ export function findTraitor(parchmentCode) {
   // Loop through the list of traitors and check if their name exists as a substring in the code
   for (let name of potentialTraitors) {
     //Holding the name of each potential traitor as a variable
-    // if (parchmentCode.includes("name:", name)) {
-    if (parchmentCode.includes(name)) {
+    if (nameInParchemt(name, parchmentCode)) {
       //Checking if the name exists as a substring in the code
-      console.log(`Parchement code ${parchmentCode} does include ${name}`);
+      // console.log(`Parchement code ${parchmentCode} does include ${name}`);
       return name;
     }
-    console.log(`Parchement code ${parchmentCode} does NOT include ${name}`);
+    // console.log(`Parchement code ${parchmentCode} does NOT include ${name}`);
   }
-  console.log('Not Found - throwing Error');
+  // console.log('Not Found - throwing Error');
   throw new Error("No traitor found");
 }
 
-// let parchmentCode =
-//   "hL2I@k4jjA7zD98n6L!L1mFhP!aVh3X4a8e9R92sYtkdZ#8Fi!P-87cr78ngL1i8n9d6aT3w7tX9L1";
+// Checks if each letter of the name exists in the parchment
+function nameInParchemt(name, parchmentCode) {
+  for (var oneLetter of name) {
+    if (!parchmentCode.includes(oneLetter)) {
+      return false;
+    }
+  }
+  return true;
+}
